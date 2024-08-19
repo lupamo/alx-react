c755ab6280345b1dd899faea16b85d80d0074fc5
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-08-19 23:53:29 +0300

    webpack 2

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+  resolve: {
+    alias: {
+      'jQuery': 'jquery',
+    }
+  }
+};
