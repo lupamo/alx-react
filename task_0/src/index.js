fccfbd95d00a8bbe346818dc57dbac58267291ff
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-08-19 21:30:24 +0300

    webpack

--- task_0/src/index.js
+++
@@ -1,7 +0,0 @@
-const $ = require('jquery');
-
-function paragraph() {
-	$('<p>').text('Holberton Dashboard').appendTo('body');
-	$('<p>').text('Dashboard data for the students').appendTo('body');
-	$('<p>').text('Copyright - Holberton School').appendTo('body');
-}
