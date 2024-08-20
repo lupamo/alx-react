163bf47280ea364489eb9866156293a01cc81c7f
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-08-20 08:27:18 +0300

    task_2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from "jQuery";
+import _ from "lodash";
+
+$(document).ready(function() {
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<button>Click here to get started</button>');
+	$('body').append("<p id='count'></p>");
+	$('button').on('click', debouncedUpdateCounter);
+	$('body').append('Copyright - Holberton School');
+});
+let count = 0;
+
+function updateCounter() {
+	count++;
+	$('#count').text(`${count} clicks on the button`);
+}
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
