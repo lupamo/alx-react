809500c745bc92f33c587714e3a4becd10d1ab47
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-08-23 17:13:03 +0300

    dashboard

---
+++ 0x01-react_intro/task_1/src/utils.js
@@ -0,0 +1,14 @@
+function getFullYear() {
+	const currentDate = new Date();
+	return currentDate.getFullYear();
+}
+
+function getFooterCopy(isIndex) {
+	if (bool === True) {
+		return("Holberton School");
+	} else {
+		return("Holberton School main dashboard");
+	}
+}
+
+export default getFullYear;
---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlfor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+
+          <button type="submit">OK</button>
+        </form>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
