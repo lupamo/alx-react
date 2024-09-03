c1da4b0e7a7cb0d751021508aab52f8604231a6c
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-09-03 21:06:44 +0300

    responsive

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+	body: {
+		height: '400px',
+		fontSize: '1.2rem',
+	},
+	form : {
+		display: 'flex',
+		flexDirection: 'column',
+	},
+	input: {
+		marginBottom: '10px',
+	},
+	button: {
+		width: 'fit-content',
+		padding: '7px 15px',
+		marginTop: '10px',
+	},
+	'@media (max-width: 900px)': {
+    body: {
+      fontSize: '1rem',
+      padding: '10px',
+    },
+    input: {
+      width: '100%',
+    },
+    button: {
+      alignSelf: 'flex-start',
+    }
+},
+});
+function Login() {
+	return (
+		<div className={css(styles.body)}>
+			<p>Login to access the full dashboard</p>
+		<form className={css(styles.form)}>
+			<label htmlFor="email">Email:</label>
+			<input className={css(styles.input)} type="email" id="email" name="email" />
+
+			<label htmlFor="password">Password:</label>
+			<input className={css(styles.input)} type="password" id="password" name="password" />
+
+			<button  className={css(styles.button)} type="submit">OK</button>
+		</form>
+	</div>
+	);
+}
+
+export default Login;
---
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem'
+import { getLatestNotification } from "../utils/utils.js";
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+	body: {
+		border: '2px dashed red',
+		padding: '20px'
+	}
+});
+class Notification extends Component {
+	render() {
+		const displayDrawer = true;
+		const handleClick = () => {
+			console.log('Close button has been clicked');
+		  };
+		return (
+			<>
+				<div className={css(styles.body)}>
+					<p>Your notifications</p>
+					{displayDrawer && (
+					<div className="Notifications">
+						<p>Here is the list of notifications</p>
+						<ul>
+							<NotificationItem type="info" value="New course available" />
+							<NotificationItem type="default" value="New resume available" />
+							<NotificationItem type="urgent" html={getLatestNotification()} />
+						</ul>
+						<button
+							style={{
+							position: 'absolute',
+							top: '20px',
+							right: '20px',
+							backgroundColor: 'none',
+							border: 'none',
+							cursor: 'pointer',
+							}}
+							aria-label="close"
+							onClick={handleClick}
+						>
+							<img src={closeIcon} alt="close-icon" style={{ width: '15px', height: '15px' }} />
+						</button>
+					</div>
+				)}
+				
+				</div>
+				
+			</>
+		);
+	};
+}
+
+export default Notification;
