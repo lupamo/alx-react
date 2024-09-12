a32048ed62e5c1cafbbc8645d24c71b6c5af1340
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2024-09-12 12:06:59 +0300

    map

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,5 @@
 import { fromJS } from 'immutable';
 
-/** function that convertes object parameter to an immutable map */
-
 export default function getImmutableObject(object) {
 	return  fromJS(object);
 }
--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,17 +1,6 @@
-#!/usr/bin/node
+import { Map } from 'immutable';
 
-/**  conversion of objects to immutable maps*/
-
-const Immutable = require('immutable');
-
-/** 
- * function that convertes object parameter to an immutable 
- * map using Map
-**/
-
-function getImmutableObject(object) {
+ export default function getImmutableObject(object) {
 	const mutatedObj =  Immutable.Map(object);
 	return(mutatedObj);
 }
-
-module.exports = getImmutableObject;
